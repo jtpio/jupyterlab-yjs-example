9916db52def2e151b29743e6712fe4f413c441c6
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-01-13 20:39:00 +0100

    Add server handler

--- jupyterlab-yjs-example/__init__.py
+++ jupyterlab-yjs-example/__init__.py
@@ -2,12 +2,14 @@
 import json
 import os.path as osp
 
+from .handlers import setup_handlers
 from ._version import __version__
 
 HERE = osp.abspath(osp.dirname(__file__))
 
 with open(osp.join(HERE, 'labextension', 'package.json')) as fid:
     data = json.load(fid)
+
 
 def _jupyter_labextension_paths():
     return [{
@@ -16,4 +18,19 @@ def _jupyter_labextension_paths():
     }]
 
 
+def _jupyter_server_extension_points():
+    return [{
+        "module": "jupyterlab_yjs_example"
+    }]
+
+
+def _load_jupyter_server_extension(server_app):
+    """Registers the API handler to receive HTTP requests from the frontend extension.
+    Parameters
+    ----------
+    lab_app: jupyterlab.labapp.LabApp
+        JupyterLab application instance
+    """
+    setup_handlers(server_app.web_app)
+    server_app.log.info("Registered HelloWorld extension at URL path /jupyterlab_yjs_example")
 
